b714df383e62128b6edf866c014c8b7f712fe49f
samuelcpr <samuelcpr464@gmail.com>
2022-08-02 15:06:34 -0300

    Chore: integration code

---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+	constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+
+	handle(request: Request, response: Response): Response {
+		try {
+			const { user_id } = request.params;
+			const user = this.turnUserAdminUseCase.execute({
+				user_id: user_id.toString(),
+			});
+
+			return response.status(200).json(user);
+		} catch (error) {
+			return response.status(404).json({
+				error: error.message,
+			});
+		}
+	}
+}
+
+export { TurnUserAdminController };
